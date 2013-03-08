bd07413db0ebdc2bbb6a84437878fa62a494d6f9
svjson <sven.johansson@patternjuggler.se>
2013-03-08 10:43:18 +0100

    Moved view js to separate folder

--- grunt.js
+++ grunt.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     meta: {
-      version: '0.0.2',
+      version: '0.0.3',
       banner: '/*! TabularBells - v<%= meta.version %> - ' +
         '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
         '* http://www.github.com/svjson/TabularBells/\n' +
